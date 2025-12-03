1ba546433c1dbad8f0b8db1ef99d64a23ecb73f1
Husen <husen.rusdiansyah@esdm.go.id>
2025-12-03 08:28:48 +0700

    Tambah function hitung_umur

--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import "fmt"
 
+func hitung_umur(tahun int) int {
+	return 2025 - tahun
+
+}
 func main() {
-	fmt.Println("Hello there")
+	fmt.Println("Starting...")
+	umur := hitung_umur(1985)
+	fmt.Println("Umur anda:", umur)
 
 }
