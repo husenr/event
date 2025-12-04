6da3119e17c8b3901df8af647c0905155b6e3ff8
Husen <husen.rusdiansyah@esdm.go.id>
2025-12-04 11:10:26 +0700

    Tambah function pengurangan dan input string/integer

--- main.go
+++ main.go
@@ -1,14 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func hitung_umur(tahun int) int {
-	return 2025 - tahun
+func hitung_umur(tanggal_lahir time.Time) int {
+	t := time.Now()
+
+	year := tanggal_lahir.Year()
+	selisih_tahun := t.Year() - year
+
+	return selisih_tahun
+}
+func pengurangan(angka1 int, angka2 int) int {
+
+	return angka1 - angka2
 
 }
 func main() {
+	var tanggal_lahir string
+	var rata_umur int
+	layout := "2006-01-02"
+
 	fmt.Println("Starting...")
-	umur := hitung_umur(1985)
-	fmt.Println("Umur anda:", umur)
+	fmt.Println("masukkan tanggal lahir :")
+	fmt.Scanln(&tanggal_lahir)
+	tanggal, err := time.Parse(layout, tanggal_lahir)
+	if err != nil {
+		fmt.Println("Error Parsing:", err)
+
+	}
+	umur := hitung_umur(tanggal)
+	fmt.Println("Umur anda:", umur, "Tahun")
+	fmt.Print("Masukkan rata-rata umur :")
+	fmt.Scan(&rata_umur)
+	fmt.Println("Jika rata-rata umur = ", rata_umur, "tahun, maka...")
+	sisa := pengurangan(rata_umur, umur)
+	fmt.Println("Sisa umur anda :", sisa, "tahun")
 
 }
